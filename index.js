b0b6fe64e6915856b186e81546f8ce6e61cfc8f8
Zainab Saud <zai99nb@gmail.com>
2020-06-06 18:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let data = {
 name : "Steve",
 email : "steve@steve.com"
 };
+<<<<<<< HEAD
 let rando = Math.ceil( Math.random() * 1000 );
+=======
+let token = null;
+>>>>>>> f7a36ff63d2e498bc50ea965f0b9556f8e1b4a04
 let config = {
   method:"POST",
   headers:{
@@ -15,11 +19,28 @@ let config = {
 function submitData(name,email){
 return fetch('http://localhost:3000/users',config)
 .then(res=>res.json())
+<<<<<<< HEAD
 .then(object=>
 document.body.innerHTML =(object["id"])
+=======
+.then(json=>
+ {
+          id: rando
+        }
+//document.body.innerHTML=(`${id}`)
+>>>>>>> f7a36ff63d2e498bc50ea965f0b9556f8e1b4a04
 )
 .catch(function(error){
  document.body.innerHTML=(error.message)
 });
 }
 
+<<<<<<< HEAD
+=======
+function renderData(){
+  let name = document.getElementById("user-name");
+  let email = document.getElementById("email");
+  let submit = document.getElementById("submit")
+  
+}
+>>>>>>> f7a36ff63d2e498bc50ea965f0b9556f8e1b4a04
